38b23d3ccc14d6e70bf04ec8c5d1ca6e69be36f8
SFlorent <leyygaming@live.fr>
2019-04-27 14:42:46 +0200

    merge emdbedTomcat

--- embedtomcat/src/main/java/fr/univubs/inf1603/mahjong/interfaces/web/embedtomcat/Main.java
+++ embedtomcat/src/main/java/fr/univubs/inf1603/mahjong/interfaces/web/embedtomcat/Main.java
@@ -1,5 +1,7 @@
 package fr.univubs.inf1603.mahjong.interfaces.web.embedtomcat;
 
+import fr.univubs.inf1603.mahjong.sapi.SapiManager;
+import fr.univubs.inf1603.mahjong.sapi.impl.SapiManagerImpl;
 import java.io.File;
 import javax.servlet.ServletContext;
 
@@ -16,10 +18,10 @@ public class Main {
      * @throws Exception
      */
     public static void main(String[] args) throws Exception {
-        EmbeddedTomcat embeddedTomcat = new EmbeddedTomcat(8081) {
+        EmbeddedTomcat embeddedTomcat = new EmbeddedTomcat(8082) {
             @Override
             protected void initApplicationContext(ServletContext servletContext) {
-                
+                servletContext.setAttribute("fr.univubs.inf1603.sapi.SapiManagerImpl", new SapiManagerImpl());
             }
         };
         //Tomcat tomcat = embeddedTomcat.getTomcat();
